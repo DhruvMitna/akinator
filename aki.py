24a1b7866a621012fe366aaa5b948c87429757da
DhruvMitna19 <dhruvmitna9@gmail.com>
2021-09-26 17:46:30 +0530

    added comments

--- aki.py
+++ aki.py
@@ -1,10 +1,14 @@
+# Importing the necessary functions from modules.
 from akinator import Akinator, CantGoBackAnyFurther
 
+# Getting the values from the user.
 questions = int(input("Enter the maximum number of questions: "))
 
+# Initializing akinator.
 aki = Akinator()
 start = aki.start_game()
 
+# Asking the user questions until the limit is reached.
 while aki.progression < questions + 1:
 
     answer = input(f"{start}: ")
@@ -25,8 +29,10 @@ while aki.progression < questions + 1:
 
 aki.win()
 
+# Asking the user if the guess is correct.
 correct = input(f"It's {aki.first_guess['name']}!\n\n{aki.first_guess['description']} ({aki.first_guess['absolute_picture_path']})\nIs this correct? ").lower()
 
+# Determining who won.
 if correct in ["yes", "y"]:
 
     print("Akinator won!")
